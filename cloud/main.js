8d17c119f2ebeaf9d3713a8b071473473b8c33ea
Timothy Dillman <tim@launchpeer.com>
2017-06-09 16:42:23 -0400

    updated to include a csv parser and background worked.  Still need to get the ingestion of the csv file completed

--- cloud/main.js
+++ cloud/main.js
@@ -1,11 +1,99 @@
 /**
  * Place Parse Cloud Code Here
  */
+const gFinance = require('google-finance');
+const csv = require('csv');
 
 Parse.Cloud.define('test_push_services', function( req, res) {
     Parse.Push.send({
         data: {
             alert:"This is a test from Parse Server"
+        }
+    })
+    return res.success(null);
+});
+
+Parse.Cloud.define('stock_rating', function( req, res ) {
+    console.log(req);
+    if(!req.params.symbol) { return res.error('Did not provide symbol'); }
+    gFinance.historical({
+        symbol: req.params.symbol,
+        from: '2014-01-01',
+        to: '2017-12-31'
+    })
+    .then(data => {
+        return res.success(data);
+    })
+    .catch(err => {
+        return res.error(err);
+    });
+});
+
+Parse.Cloud.define('update_symbol_table', function( req, res ) {
+    console.log(req);
+    var query = new Parse.Query()
+    return res.success(req);
+});
+
+Parse.Cloud.define('all_symbol_files', function( req, res ){
+    var SymbolFiles = Parse.Object.extend('SymbolFiles');
+    var query = new Parse.Query(SymbolFiles);
+    query.useMasterKey = true;
+    query.find({
+        useMasterKey: true,
+        success: function(objects) {
+            return res.success(objects);
+        },
+        error: function(err) {
+            console.log(err);
+            return res.error(err);
+        }
+    });
+});
+
+/**
+ * Parse Cloud Jobs
+ */
+
+Parse.Cloud.job('ingest_symbols', function( req, status ) {
+    var params = req.params;
+    var headers = req.headers;
+    var log = req.log;
+    status.message('started ingesting symbol files');
+    var SymbolFiles = Parse.Object.extend("SymbolFiles");
+    var query = new Parse.Query(SymbolFiles);
+    query.find({
+        useMasterKey: true,
+        success: function(objects) {
+            console.log(objects);
+            console.log(objects.length);
+            if(objects.length > 0) {
+                console.log("symbol files are available");
+                let url = objects[0].get("csv_symbol");
+                
+                
+                Parse.Cloud.httpRequest({url: url.url(),
+                    success: function(object) {
+                        
+                        csv.parse(object.buffer.toString(), {auto_parse: true, relax: true, relax_column_count: true}, (err, result) => {
+                            console.log(err);
+                            console.log(result);
+                            
+                            status.success(result);
+                        });
+                        
+                        
+                    },
+                    error: function(err) {
+                        status.error(err);
+                    }
+                });
+            }
+            
+        },
+        error: function(err) {
+            console.log(err);
+            status.error(err);
         }
     });
 });
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/parse-example',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/keu-research',
     PARSE_APP_ID: process.env.PARSE_APP_ID || 'your_app_id',
     PARSE_MASTER_KEY: process.env.PARSE_MASTER_KEY || '?Ag4M)ZA*BBpIGL!XIqCe6acB&rcmL',
     PARSE_SERVER_URL: process.env.PARSE_SERVER_URL || 'http://localhost:1337/parse',
